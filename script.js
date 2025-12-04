bed837ca4f6f515b9c7101ea98e4bc85e613fa47
muhammad-ahmad-sudo <fiverr.pro66@gmail.com>
2025-12-04 02:10:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const width = 600;
-const height = 350;
-const margin = { top: 30, right: 30, bottom: 40, left: 50 };
+const width = 800;
+const height = 450;
+const margin = { top: 40, right: 40, bottom: 50, left: 60 };
 
 const svg = d3
   .select("#chart")
@@ -14,41 +14,49 @@ const tooltip = d3
   .attr("class", "tooltip");
 
 function drawChart(type, data) {
-  svg.selectAll("*").remove(); // clear previous chart
+  svg.selectAll("*").remove();
 
-  const x = d3
-    .scaleBand()
+  const x = d3.scaleBand()
     .domain(data.map(d => d.Year))
     .range([margin.left, width - margin.right])
-    .padding(0.4);
+    .padding(0.3);
 
-  const y = d3
-    .scaleLinear()
+  const y = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.Sales)])
     .nice()
     .range([height - margin.bottom, margin.top]);
 
   // X Axis
   svg.append("g")
-    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .attr("transform", `translate(0, ${height - margin.bottom})`)
     .call(d3.axisBottom(x));
 
   // Y Axis
   svg.append("g")
-    .attr("transform", `translate(${margin.left},0)`)
+    .attr("transform", `translate(${margin.left}, 0)`)
     .call(d3.axisLeft(y));
 
+  // -------------------------
+  // BAR CHART
+  // -------------------------
   if (type === "bar") {
     svg.selectAll(".bar")
       .data(data)
       .enter()
       .append("rect")
       .attr("x", d => x(d.Year))
-      .attr("y", d => y(d.Sales))
       .attr("width", x.bandwidth())
-      .attr("height", d => height - margin.bottom - y(d.Sales))
+      .attr("y", height - margin.bottom) // start from bottom
+      .attr("height", 0)
       .attr("fill", "steelblue")
-      .on("mousemove", function (event, d) {
+      .transition()
+      .duration(800)
+      .ease(d3.easeCubic)
+      .attr("y", d => y(d.Sales))
+      .attr("height", d => height - margin.bottom - y(d.Sales));
+
+    svg.selectAll("rect")
+      .on("mousemove", (event, d) => {
         tooltip
           .style("opacity", 1)
           .style("left", event.pageX + 10 + "px")
@@ -56,28 +64,51 @@ function drawChart(type, data) {
           .html(`<strong>${d.Year}</strong><br>Sales: ${d.Sales}`);
       })
       .on("mouseout", () => tooltip.style("opacity", 0));
+  }
 
-  } else if (type === "line") {
+  // -------------------------
+  // LINE CHART + ANIMATION
+  // -------------------------
+  if (type === "line") {
     const line = d3.line()
       .x(d => x(d.Year) + x.bandwidth() / 2)
-      .y(d => y(d.Sales));
+      .y(d => y(d.Sales))
+      .curve(d3.curveMonotoneX);
 
-    svg.append("path")
+    const path = svg.append("path")
       .datum(data)
       .attr("fill", "none")
       .attr("stroke", "steelblue")
-      .attr("stroke-width", 2)
+      .attr("stroke-width", 3)
       .attr("d", line);
 
+    // Animate path drawing
+    const length = path.node().getTotalLength();
+
+    path
+      .attr("stroke-dasharray", `${length} ${length}`)
+      .attr("stroke-dashoffset", length)
+      .transition()
+      .duration(1200)
+      .ease(d3.easeCubic)
+      .attr("stroke-dashoffset", 0);
+
+    // Add points with animation
     svg.selectAll("circle")
       .data(data)
       .enter()
       .append("circle")
       .attr("cx", d => x(d.Year) + x.bandwidth() / 2)
       .attr("cy", d => y(d.Sales))
-      .attr("r", 5)
+      .attr("r", 0) // start small
       .attr("fill", "red")
-      .on("mousemove", function (event, d) {
+      .transition()
+      .delay((d, i) => i * 150)
+      .duration(500)
+      .attr("r", 6);
+
+    svg.selectAll("circle")
+      .on("mousemove", (event, d) => {
         tooltip
           .style("opacity", 1)
           .style("left", event.pageX + 10 + "px")
